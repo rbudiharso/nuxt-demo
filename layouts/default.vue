2e3d2bb465fe202a8e7d9e7397addf56541c36fb
Rahmat Budiharso <rbudiharso@gmail.com>
2018-03-09 19:07:44 +0700

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <nuxt/>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 class="title">
+    USERS
+  </h1>
+</template>
+
+<script>
+import axios from '~/plugins/axios'
+
+export default {
+  async asyncData () {
+    let { data } = await axios.get('/api/users')
+    return { users: data }
+  },
+  head () {
+    return {
+      title: 'Users'
+    }
+  }
+}
+</script>
